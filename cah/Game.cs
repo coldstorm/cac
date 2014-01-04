7d132250961014151167bd0951892a718cd2aafd
Express50 <press.ex50@gmail.com>
2014-01-04 17:53:54 -0500

    Add Game class

---
+++ cah/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cah
+{
+    public class Game
+    {
+        private List<Player> _players;
+        private GameState _state;
+
+        public Game(string[] settings)
+        {
+            this._state = GameState.None;
+        }
+
+        public void Start()
+        {
+            this._state = GameState.Lobby;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (!this._players.Contains(player))
+            {
+                this._players.Add(player);
+            }
+        }
+
+        public void RemovePlayer(Player player)
+        {
+            if (this._players.Contains(player))
+            {
+                this._players.Remove(player);
+            }
+        }
+    }
+}
---
+++ cah/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cah
+{
+    public enum GameState
+    {
+        None = 0,
+        Lobby,
+        Dealing,
+        Playing,
+        Judging
+    }
+}
