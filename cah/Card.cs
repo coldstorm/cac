06a0af617cfc5669c145d7d988c9c01a524712d4
Express50 <press.ex50@gmail.com>
2014-01-04 17:30:24 -0500

    Add Card and Player classes

---
+++ cah/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cah
+{
+    public class Card
+    {
+        public string Expansion;
+        public string Text;
+
+        public Card(string expansion, string text)
+        {
+            this.Expansion = expansion;
+            this.Text = text;
+        }
+    }
+}
---
+++ cah/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetIRC;
+
+namespace cah
+{
+    public class Player
+    {
+        private User _user;
+        public List<WhiteCard> Hand;
+
+        public Player(User user)
+        {
+            this._user = user;
+            this.Hand = new List<WhiteCard>();
+        }
+    }
+}
