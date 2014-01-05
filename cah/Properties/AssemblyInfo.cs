f00e27d101a88504e56e6451da710995d367a355
Express50 <press.ex50@gmail.com>
2014-01-04 22:30:34 -0500

    Rename namespace to cac

--- cah/Properties/AssemblyInfo.cs
+++ cah/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("cah")]
+[assembly: AssemblyTitle("cac")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("cah")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2014")]
+[assembly: AssemblyCompany("Coldstorm")]
+[assembly: AssemblyProduct("cac")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
